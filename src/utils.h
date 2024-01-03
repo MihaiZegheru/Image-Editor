d0795583fe33f38e31bc8138a28cca446271e918
MihaiZegheru <mihai.z.2005@gmail.com>
2024-01-03 13:16:04 +0200

    Added utils_count_words
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -8,5 +8,6 @@ int64_t utils_string_to_int64(char *str);
 int64_t utils_max_int64(uint64_t a, uint64_t b);
 double utils_clamp(double value, double min, double max);
 void utils_swap_pointers(void **a, void **b);
+size_t utils_count_words(char *str);
 
 #endif // UTILS_H__
